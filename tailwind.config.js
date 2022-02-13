8041c428193ab84597e819171c2dd4b6a273e614
David Fabian <davidhfabian@gmail.com>
2022-02-13 16:57:02 -0300

    feat: add font and setting theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
-const colors = require("tailwindcss/colors");
+const customThemeColors = (theme) => {
+  return {
+    ...theme("colors"),
+    primary: '#CC2D4A',
+    secondary: '#8FA206',
+    tertiary: '#61AEC9',
+  };
+}
 
 module.exports = {
   mode: "jit",
@@ -6,12 +13,30 @@ module.exports = {
   darkMode: false,
   content: [],
   theme: {
-    extend: {},
-    colors: {
-      primary: colors.purple,
-      secondary: colors.fuchsia,
-      'midnight': '#121063',
-    }
+    extend: {
+      backgroundImage: {
+        'sanFrancisco': "url('../assets/img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('../assets/img/sanFranciscoDesktop.jpg')",
+        'yosemite': "url('../assets/img/yosemite.jpg')",
+        'LA': "url('../assets/img/LA.jpg')",
+        'seattle': "url('../assets/img/seattle.jpg')",
+        'new_york': "url('../assets/img/new_york.jpg')",
+        'norway': "url('../assets/img/norway.jpg')",
+        'sydney': "url('../assets/img/sydney.jpg')",
+        'miami': "url('../assets/img/miami.jpg')",
+        'switzerland': "url('../assets/img/switzerland.jpg')",
+        'bali': "url('../assets/img/bali.jpg')",
+        'chicago': "url('../assets/img/chicago.jpg')",
+        'europe': "url('../assets/img/europe.jpg')",
+        'iceland': "url('../assets/img/iceland.jpg')",
+      },
+      backgroundColor: customThemeColors,
+      textColor: customThemeColors,
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+    colors: {}
   },
   plugins: [
     require("@tailwindcss/aspect-ratio"),

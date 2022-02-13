53a4829055e021a2db50be1440fc2e6c6d173288
David Fabian <davidhfabian@gmail.com>
2022-02-13 04:16:27 -0300

    feat: setup project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: "jit", // enable tailwind just in time
+  purge: ["./index.html"],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
